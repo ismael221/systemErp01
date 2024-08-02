93c8a473bceb1de3d5d575e52a81c0f82e18d4c6
Ismael Nunes <ismaeldenunes@gmail.com>
2024-08-02 16:34:50 -0300

    Cors allowed

--- src/main/java/com/ismael/systemErp01/auth/AuthenticationController.java
+++ src/main/java/com/ismael/systemErp01/auth/AuthenticationController.java
@@ -3,29 +3,23 @@ package com.ismael.systemErp01.auth;
 import com.ismael.systemErp01.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthenticationController {
 
     private  final AuthenticationService service;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody User request
-    ){
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request){
         return  ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody AuthenticationRequest request
-    ){
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
        return ResponseEntity.ok(service.authenticate(request));
     }
 }
